527cfb98641b21a45dd0eab81c06e4529ebb5338
Le Trong Huynh <letronghuynh@Les-MacBook-Air.local>
2025-12-08 09:16:01 +0700

    1231asdasd

--- controllers/shop/order-controller.js
+++ controllers/shop/order-controller.js
@@ -75,9 +75,8 @@ const createOrder = async (req, res) => {
           // and then calls backend capture endpoint with orderId stored in sessionStorage
           // return_url: "http://localhost:5173/shop/paypal-return",
           // cancel_url: "http://localhost:5173/shop/home",
-
-          return_url: "storetechnology.vercel.app/shop/paypal-return",
-          cancel_url: "storetechnology.vercel.app/shop/paypal-cancel",
+          return_url: "https://storetechnology.vercel.app/shop/paypal-return",
+          cancel_url: "https://storetechnology.vercel.app/shop/paypal-cancel",
         },
         transactions: [
           {
